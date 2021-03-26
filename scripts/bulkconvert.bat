b922abb51b48caf385dc6660dd79af0d7ea97b26
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-03-26 12:05:23 -0400

    Update bulkconvert.bat

--- scripts/bulkconvert.bat
+++ scripts/bulkconvert.bat
@@ -1,4 +1,10 @@
-for %%i in (*.veg) do (
-	python3 msvpvf.py -i "%%i" --version 13 --type veg
+if "%1" == "" (
+echo usage: %0 ^<version^>
+exit /b
 )
+
+for %%i in (*.veg) do (
+	python3 msvpvf.py -i "%%i" --version %1 --type veg
+)
+
 pause
