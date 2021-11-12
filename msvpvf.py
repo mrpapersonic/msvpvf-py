a03342761b3da09538da87740f4ef59a22c7d4d4
Paper <37962225+mrpapersonic@users.noreply.github.com>
2021-11-12 11:28:49 -0500

    Implement GUI

--- msvpvf.py
+++ msvpvf.py
@@ -1,101 +1,60 @@
-import struct, sys, argparse, os
-from pathlib import Path
+import sys, argparse, os
 
-def main(argv):
-    global inputfile
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help="input file", metavar='<input>', required=True)
-    parser.add_argument('-o', '--output', help="output file, defaults to '(MS,VEG)_(version)_(original name).(vf,veg)'", metavar='<output>')
-    parser.add_argument('--version', help="output file version", metavar='<version>')
-    parser.add_argument('--type', help="output file type, vf/veg", metavar='<type>')
-    args = parser.parse_args()
+def prog_type(byte):
+    if byte == int("F6", 16):
+        return "Movie Studio"
+    elif byte == int("EF", 16):
+        return "VEGAS Pro"
+    else:
+        return "Unknown"
+
+def type_hex(type):
+    if type == "vf" or type == 1:
+        return "MS", [int("F6", 16), int("1B", 16), int("3C", 16), int("53", 16), int("35", 16), int("D6", 16), int("F3", 16), int("43", 16), int("8A", 16), int("90", 16), int("64", 16), int("B8", 16), int("87", 16), int("23", 16), int("1F", 16), int("7F", 16)]
+    elif type == "veg" or type == 0:
+        return "PRO", [int("EF", 16), int("29", 16), int("C4", 16), int("46", 16), int("4A", 16), int("90", 16), int("D2", 16), int("11", 16), int("87", 16), int("22", 16), int("00", 16), int("C0", 16), int("4F", 16), int("8E", 16), int("DB", 16), int("8A", 16)]
+    else:
+        return
+
+def main(args):
     if args.input:
         inputfile = args.input
     if os.path.exists(inputfile):
-        fp = open(inputfile, "rb")
-        test = bytearray(fp.read())
-        fp.close()
+        with open(inputfile, "rb") as fp:
+            test = bytearray(fp.read())
     else:
         print("Input file doesn't exist!")
         exit()
-    if test[0x18] == int("F6", 16):
-        project = "Movie Studio "
-    elif test[0x18] == int("EF", 16):
-        project = "VEGAS Pro "
-    else:
-        project = "Unknown "
-    print("Project file version: " + project + str(test[0x46]) + ".0")
+    project = prog_type(test[0x18])
+    print("Project file version: " + project + " " + str(test[0x46]) + ".0")
     if not args.version:
         answer = ""
     else:
         answer = args.version
     while answer not in ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"]:
-        print("Which version of VEGAS would you like it to be spoofed to? [8-18]: ", end = "")
-        answer = input().lower()
+        answer = str(input("Which version of VEGAS would you like it to be spoofed to? [8-18]: "))
     if not args.type:
         answer2 = ""
         while answer2 not in ["veg", "vf"]:
-            print("Would you like it to be VEGAS Pro or Movie Studio? [veg,vf]: ", end = "")
+            print("Would you like it to be VEGAS Pro or Movie Studio? [veg,vf]: ", end="")
             answer2 = input().lower()
     else:
         answer2 = args.type
-    if answer2 == "vf":
-        filename_prefix = "MS"
-        test[0x18] = int("F6", 16)
-        test[0x19] = int("1B", 16)
-        test[0x1A] = int("3C", 16)
-        test[0x1B] = int("53", 16)
-        test[0x1C] = int("35", 16)
-        test[0x1D] = int("D6", 16)
-        test[0x1E] = int("F3", 16)
-        test[0x1F] = int("43", 16)
-        test[0x20] = int("8A", 16)
-        test[0x21] = int("90", 16)
-        test[0x22] = int("64", 16)
-        test[0x23] = int("B8", 16)
-        test[0x24] = int("87", 16)
-        test[0x25] = int("23", 16)
-        test[0x26] = int("1F", 16)
-        test[0x27] = int("7F", 16)
-    elif answer2 == "veg":
-        filename_prefix = "PRO"
-        test[0x18] = int("EF", 16)
-        test[0x19] = int("29", 16)
-        test[0x1A] = int("C4", 16)
-        test[0x1B] = int("46", 16)
-        test[0x1C] = int("4A", 16)
-        test[0x1D] = int("90", 16)
-        test[0x1E] = int("D2", 16)
-        test[0x1F] = int("11", 16)
-        test[0x20] = int("87", 16)
-        test[0x21] = int("22", 16)
-        test[0x22] = int("00", 16)
-        test[0x23] = int("C0", 16)
-        test[0x24] = int("4F", 16)
-        test[0x25] = int("8E", 16)
-        test[0x26] = int("DB", 16)
-        test[0x27] = int("8A", 16)
-    else:
-        print("Invalid type!")
-        exit()
+    filename_prefix, typehex = type_hex(answer2)
+    test[0x18:0x27] = typehex
     test[0x46] = int(answer)
-    filename_wo_ext = Path(inputfile).with_suffix('')
+    filename_wo_ext = os.path.splitext(inputfile)[0]
     if args.output:
         outputfile = args.output
     else:
         outputfile = filename_prefix + "_V" + answer + "_" + str(filename_wo_ext) + "." + answer2
-    if os.path.exists(outputfile):
-        answer3 = ""
-        while answer3 not in ["y", "n", "yes", "no"]:
-            print(f"{outputfile} already exists. Overwrite it? [y/n]: ", end = "")
-            answer3 = input().lower()
-        if answer3 in ["y", "yes"]:
-            os.remove(outputfile)
-        else:
-            sys.exit()
-    target = open(outputfile, 'wb')
-    target.write(test)
-    target.close()
-
+    with open(outputfile, 'wb') as target:
+    	target.write(test)
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help="input file", metavar='<input>', required=True)
+    parser.add_argument('-o', '--output', help="output file, defaults to '(MS,VEG)_(version)_(original name).(vf,veg)'", metavar='<output>')
+    parser.add_argument('-v', '--version', help="output file version", metavar='<version>')
+    parser.add_argument('-t', '--type', help="output file type, vf/veg", metavar='<type>')
+    args = parser.parse_args()
+    main(args)
